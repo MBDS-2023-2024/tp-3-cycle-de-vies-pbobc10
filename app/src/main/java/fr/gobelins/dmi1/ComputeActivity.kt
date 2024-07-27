c60ad5c1dd0984c6160f296ca3f45960266594d4
pbobc10 <pbobc10@gmail.com>
2024-07-27 07:23:23 -0400

    Tâche 1: Afficher le résultat de l'opération dans le TextView

--- app/src/main/java/fr/gobelins/dmi1/ComputeActivity.kt
+++ app/src/main/java/fr/gobelins/dmi1/ComputeActivity.kt
@@ -1,15 +1,43 @@
 package fr.gobelins.dmi1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class ComputeActivity : AppCompatActivity() {
+    lateinit var edtFirstOperand: TextInputEditText
+    lateinit var edtSecondOperand: TextInputEditText
+    lateinit var btnCompute: Button
+    lateinit var tvResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.compute_activity)
-
+        init()
         val operation = intent.getStringExtra("operation") ?: "ADD"
 
+        btnCompute.setOnClickListener {
+            // if the text cannot be converted to a Double, toDoubleOrNull() will return null
+            // the ?: (elvis) operator will provide a default value of 0.0.
+            var operand1 = edtFirstOperand.text.toString().toDoubleOrNull() ?: 0.0
+            var operand2 = edtSecondOperand.text.toString().toDoubleOrNull() ?: 0.0
+            var sum =  operand1 +  operand2
 
+            //tvResult.text = sum.toString()
+            Toast.makeText(this,"compute: $sum",Toast.LENGTH_SHORT).show()
+            tvResult.text=sum.toString()
+        }
+
+    }
+
+    private fun init(){
+        edtFirstOperand = findViewById(R.id.first_operand)
+        edtSecondOperand = findViewById(R.id.second_operand)
+        btnCompute = findViewById(R.id.btn_compute)
+        tvResult = findViewById(R.id.tv_result)
     }
 }
--- app/src/main/java/fr/gobelins/dmi1/MainActivity.kt
+++ app/src/main/java/fr/gobelins/dmi1/MainActivity.kt
@@ -1,11 +1,22 @@
 package fr.gobelins.dmi1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnHomeCompute: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnHomeCompute = findViewById(R.id.btn_home_compute)
+
+        btnHomeCompute.setOnClickListener {
+            val intent = Intent(this, ComputeActivity::class.java)
+            intent.extras?.putString("operation", "ADD")
+            startActivity(intent)
+        }
     }
 }
