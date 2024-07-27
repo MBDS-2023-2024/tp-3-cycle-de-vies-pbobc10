72cb88cc71ed85f40d697ff8eeb17a06e1b5bbe0
pbobc10 <pbobc10@gmail.com>
2024-07-27 07:56:11 -0400

    Tâche 3: Faire une recherche sur Google

--- app/src/main/java/fr/gobelins/dmi1/MainActivity.kt
+++ app/src/main/java/fr/gobelins/dmi1/MainActivity.kt
@@ -1,22 +1,31 @@
 package fr.gobelins.dmi1
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnHomeCompute: Button
+    private lateinit var btnGoogleSearch: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         btnHomeCompute = findViewById(R.id.btn_home_compute)
+        btnGoogleSearch = findViewById(R.id.btn_search)
 
         btnHomeCompute.setOnClickListener {
             val intent = Intent(this, ComputeActivity::class.java)
             intent.extras?.putString("operation", "ADD")
             startActivity(intent)
+        }
+
+        btnGoogleSearch.setOnClickListener {
+            var browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://google.fr"))
+            startActivity(browserIntent)
         }
     }
 }
