266fc3e143233fdd0718bf7f3fa0cabc4780a449
pbobc10 <pbobc10@gmail.com>
2024-07-27 08:32:00 -0400

    Tâche 5: Lancer un itinéraire

--- app/src/main/java/fr/gobelins/dmi1/MainActivity.kt
+++ app/src/main/java/fr/gobelins/dmi1/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnHomeCompute: Button
     private lateinit var btnGoogleSearch: Button
     private lateinit var btnShare: Button
+    private lateinit var btnNavigate: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         btnHomeCompute = findViewById(R.id.btn_home_compute)
         btnGoogleSearch = findViewById(R.id.btn_search)
         btnShare = findViewById(R.id.btn_share)
+        btnNavigate = findViewById(R.id.btn_navigate)
 
         btnHomeCompute.setOnClickListener {
             val intent = Intent(this, ComputeActivity::class.java)
@@ -40,6 +42,18 @@ class MainActivity : AppCompatActivity() {
             val chooser = Intent.createChooser(shareIntent,"Partager via")
 
             startActivity(chooser)
+        }
+
+        btnNavigate.setOnClickListener {
+            // Coordonnées des Papeteries Gobelins
+            val googleMapLocation = Uri.parse("google.navigation:q=48.826373,2.362527")
+            val mapIntent = Intent(Intent.ACTION_VIEW, googleMapLocation)
+
+            //Intent est configuré pour utiliser Google Maps
+            mapIntent.setPackage("com.google.android.apps.maps")
+            if (mapIntent.resolveActivity(packageManager) != null) {
+                startActivity(mapIntent)
+            }
         }
     }
 }
